e2f4956769196ca97acf08ce85715e941213edff
Mehedi hasan <mehedihasan645356@gmail.com>
2024-12-19 06:45:58 +0600

    data-get-and-add-and-showing on-client-side

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb')
 require('dotenv').config()
 
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 5000
 const app = express();
 app.use(cors())
 app.use(express.json())
@@ -21,6 +21,39 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // 
+    const jobCollection = client.db("solosphere-jobApp").collection("jobs");
+
+ // get add the jobs
+app.get('/jobs', async(req,res)=>{
+    const result = await jobCollection.find().toArray();
+    res.send(result)
+})
+// post data by requirter
+app.post("/add-jobs", async ( req , res)=>{
+     const job_Data = req.body;
+     console.log(job_Data)
+     const result = await jobCollection.insertOne(job_Data)
+      res.send(result)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
